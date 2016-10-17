70cfc4b410e996890180cc9bd7de937fca40dbbf
Kepplinger <simon.kepplinger@gmail.com>
2016-10-17 21:35:23 +0200

    Preperation for fixes

--- ConnectionCheckerNg/src/app/gui/view/overview/overview.component.ts
+++ ConnectionCheckerNg/src/app/gui/view/overview/overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Device} from "../../../classes/device";
 import {DeviceService} from "../../../devices/device.service";
 import {Observable} from "rxjs";
@@ -24,7 +24,6 @@ export class OverviewComponent implements OnInit {
       .subscribe(() => {
         this.updateDevices();
       });
-    this.updateDevices();
   }
 
   public updateDevices() {
@@ -33,9 +32,9 @@ export class OverviewComponent implements OnInit {
     this.offline = this.deviceService.getOfflineDevices();
     this.online = this.deviceService.getOnlineDevices();
 
-    var item = new HistoryItem(this.deviceService.getLastUpdate(),[this.offline,this.online]);
-      this.history_onof.push(item);
-    this.drawChart();
+    var item = new HistoryItem(this.deviceService.getLastUpdate(), [this.offline, this.online]);
+    this.history_onof.push(item);
+    this.drawCharts();
   }
 
   ngOnInit() {
@@ -43,83 +42,83 @@ export class OverviewComponent implements OnInit {
   }
 
   loadGoogleChart() {
-    google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(() => this.drawChart());
+    google.charts.load('current', {'packages': ['corechart'], 'callback': this.drawCharts()});
   }
 
-  drawChart () {
-    if(google!=null && google.visualization != null)
-    {
+  drawCharts() {
+    if (google != null && google.visualization != null) {
+      this.drawPieChart();
+      this.drawLineChart();
+    }
+  }
 
-      //######## On Off Pie Chart ################
+  drawPieChart() {
 
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'State');
+    data.addColumn('number', 'Status');
+
+    var on = 0, off = 0;
+    for (var i = 0; i < this.history_onof.length; i++) {
+      if (this.history_onof[i] != null) {
+        on += this.history_onof[i].getData()[1];
+        off += this.history_onof[i].getData()[0];
+      }
+    }
+
+    data.addRows([
+      ['Online', on / (on + off)],
+      ['Offline', off / (on + off)]
+    ]);
+
+    // Set chart options
+    var options = {
+      colors: ['#5cb85c', '#d9534f']
+    };
+
+    if (document.getElementById('onof_chart') != null) {
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.PieChart(document.getElementById('onof_chart'));
+      chart.draw(data, options);
+    }
+  }
+
+  drawLineChart() {
+    if (this.history_onof.length > 0) {
+
+      //return;
       var data = new google.visualization.DataTable();
-      data.addColumn('string', 'State');
-      data.addColumn('number', 'Status');
+      data.addColumn('date', 'Timestamp');
+      data.addColumn('number', 'Offline');
+      data.addColumn('number', 'Online');
 
-      var on = 0,off = 0;
-      for(var i = 0;i<this.history_onof.length;i++){
-        if(this.history_onof[i] != null){
-          on += this.history_onof[i].getData()[1];
-          off += this.history_onof[i].getData()[0];
+      for (var i = 0; i < this.history_onof.length; i++) {
+        if (this.history_onof[i] != null) {
+          var ba = [
+            this.history_onof[i].getTime(),
+            this.history_onof[i].getData()[1],
+            this.history_onof[i].getData()[0]
+          ];
+          data.addRow(ba);
         }
       }
 
-
-      data.addRows([
-        ['Online', on/(on+off)],
-        ['Offline', off/(on+off)]
-      ]);
-
-      // Set chart options
       var options = {
-        colors: ['#5cb85c','#d9534f']
+        colors: ['#5cb85c', '#d9534f'],
+        vAxis: {
+          maxValue: this.devices.length,
+          minValue: 0
+        }
       };
 
-      if(document.getElementById('onof_chart')!=null){
+      if (document.getElementById('history_onof_chart') != null) {
         // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('onof_chart'));
+        var chart = new google.visualization.AreaChart(document.getElementById('history_onof_chart'));
         chart.draw(data, options);
-      }
-
-
-      //####### HISTORY ON OFF CHART #############
-
-      if(this.history_onof.length>0){
-
-        //return;
-        data = new google.visualization.DataTable();
-        data.addColumn('date','Timestamp');
-        data.addColumn('number','Offline');
-        data.addColumn('number','Online');
-
-        for(var i = 0;i<this.history_onof.length;i++){
-          if(this.history_onof[i] != null){
-            var ba = [
-              this.history_onof[i].getTime(),
-              this.history_onof[i].getData()[1],
-              this.history_onof[i].getData()[0]
-            ];
-            data.addRow(ba);
-          }
-        }
-
-        options = {
-          colors: ['#5cb85c','#d9534f'],
-          vAxis:{
-            maxValue: this.devices.length,
-            minValue: 0
-          }
-        };
-
-        if(document.getElementById('history_onof_chart')!=null){
-          // Instantiate and draw our chart, passing in some options.
-          var chart = new google.visualization.AreaChart(document.getElementById('history_onof_chart'));
-          chart.draw(data, options);
-        }
       }
     }
   }
+
 
   checkloaded(): boolean {
     return !((typeof google === 'undefined') || (typeof google.visualization === 'undefined'));
